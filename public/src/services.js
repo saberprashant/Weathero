0cdb3e0c93fe33c877719aabf1d7c1bed8989ee1
saberprashant <prashantjain.pro@gmail.com>
2018-03-12 17:15:12 +0530

    changes done

--- public/src/services.js
+++ public/src/services.js
@@ -1,6 +1,17 @@
 // SERVICES
 weatherApp.service('cityService', function() {
    
-    this.city = "New York, US";
+    this.city = "Delhi, IN";
     
+});
+
+weatherApp.factory('States', function() {
+
+    var states = [
+        'Delhi, IN', 'Mumbai, IN', 'Chennai, IN', 'Agra, IN', 'Pune, IN', 'Bengaluru, IN',
+        'Hyderabad, IN','Ahmedabad, IN',
+        'New York, US', 'Colorado, US', 'Nevada, US', 'Miami, US',
+        'Ottawa, CAN', 'Ontario, CAN'
+    ]
+    return states;
 });
